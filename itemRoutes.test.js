5c6836e6b26977d5a89f8c4304ed0198a70a6e5b
Jared <jared_glenn@yahoo.com>
2023-12-01 10:34:43 -0700

    finish tests and exercise

--- itemRoutes.test.js
+++ itemRoutes.test.js
@@ -32,3 +32,44 @@ describe("GET /items", function() {
     });
 });
 
+describe("POST /items", function() {
+    test("Adds an item to the list", async function() {
+        const resp = await request(app).post('/items?name=popsicle&price=1.45');
+        expect(resp.statusCode).toBe(200);
+
+        expect(resp.body).toEqual({"added": {"name": "popsicle", "price": "1.45"}});
+    })
+})
+
+describe("GET /items/:name", function() {
+    test("Finds a single item on the list", async function() {
+        const resp = await request(app).get('/items/bread');
+        expect(resp.statusCode).toBe(200);
+
+        expect(resp.body).toEqual({
+            "name": "bread",
+            "price": 0.99
+        });
+    })
+})
+
+describe("PATCH /items/:name", function() {
+    test("Makes a change to an item", async function() {
+        const resp = await request(app).patch('/items/bread?name=bread&price=1.25');
+        expect(resp.statusCode).toBe(200);
+
+        expect(resp.body).toEqual({"updated": {
+            "name": "bread",
+            "price": 1.25
+        }});
+    })
+})
+
+describe("DELETE /items/:name", function() {
+    test("Deletes an item", async function() {
+        const resp = await request(app).delete('/items/bread');
+        expect(resp.statusCode).toBe(200);
+
+        expect(resp.body).toEqual({"message": "Deleted"});
+    })
+})
